c0d77dccadf0427b0bcdf861482e2a6cef577c1a
lixiaohan <lixh14@mails.tsinghua.edu.cn>
2017-06-03 22:21:25 +0800

    add readFromFile() and some static infomation

--- Tomasulo.java
+++ Tomasulo.java
@@ -5,14 +5,115 @@ import java.math.*;
 import java.io.*;
 import java.util.*;
 
+
+public class Tomasulo {
+
+    static String add="ADDD", sub="SUBD", multi="MULTD", div="DIVD", load="LD", store="ST";
+
+    Queue<Instruction> ins_queue;
+    Register[] registers = new Register[16];
+    ReservedStation[] stations = new ReservedStation[3+2+3+3];
+        // add*3 multi*2 load*3 store*3
+    static int base_a=0, base_m=3, base_l=5, base_s=8;
+
+    byte[] mem = new byte[4096];
+    int clock = 0;
+    int add_alu_exec=-1, multi_alu_exec=-1; // id of reserved station which holds the ins executed now
+
+    public static void main(String[] agrs)
+    throws Exception
+    {
+        Tomasulo tomasulo = new Tomasulo();
+    }
+    public Tomasulo()
+    throws Exception
+    {
+        // main func starts here
+        init();
+        boolean read = readInsFromFile("ins.txt");
+        if(!read){
+            info("Error in reading ins from file");
+            return;
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(br.readLine().length()==0){
+            clock ++;
+            info(clock);
+            // fetch ins
+
+
+            // update alu infomation. if finished, inform those waiting
+
+            // update UI
+        }
+    }
+
+
+    private void info(String x){
+        // encapsulate print function
+        System.out.println(x);
+    }
+    private void info(int x){
+        // encapsulate print function
+        System.out.println(x);
+    }
+    private void init(){
+        ins_queue = new LinkedList<>();
+        // dequeue:poll()  get head ele:peek()
+        for(int i=0; i<registers.length; i++){
+            registers[i] = new Register();
+        }
+        for(int i=0; i<stations.length; i++){
+            stations[i] = new ReservedStation();
+        }
+    }
+    private boolean readInsFromFile(String filename){
+        // return false if open fails or ins are unformatted
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+            String line = "";
+            while((line=br.readLine()) != null){
+                String[] ins = line.split(" ");
+                String[] ops = ins[1].split(",");
+                if(ins[0].equals(add) || ins[0].equals(sub) || ins[0].equals(multi) || ins[0].equals(div)){
+                    int dst = Integer.parseInt(ops[0].replace("F", ""));
+                    int op1 = Integer.parseInt(ops[1].replace("F", ""));
+                    int op2 = Integer.parseInt(ops[2].replace("F", ""));
+                    Instruction instruction = new Instruction(ins[0], dst, op1, op2);
+                    ins_queue.offer(instruction);
+                }
+                else if(ins[0].equals(load) || ins[0].equals(store)){
+                    int reg = Integer.parseInt(ops[0].replace("F", ""));
+                    int addr = Integer.parseInt(ops[1]);
+                    Instruction instruction = new Instruction(ins[0], reg, addr);
+                    ins_queue.offer(instruction);
+                }
+                else {
+                    return false;
+                }
+            }
+        }catch (IOException e){
+            info("IO error");
+            return false;
+        }
+        return true;
+    }
+}
+
 class Instruction{
     public String ins;
-    public int op1_reg_id, op2_reg_id, op3_reg_id;
-    public Instruction(String i, int op1, int op2, int op3){
+    public int dst_reg_id, op1_reg_id, op2_reg_id;
+    public int addr;
+    public Instruction(String i, int dst, int op1, int op2){
         this.ins = i;
+        this.dst_reg_id = dst;
         this.op1_reg_id = op1;
         this.op2_reg_id = op2;
-        this.op3_reg_id = op3;
+    }
+    public Instruction(String i, int reg, int addr){
+        this.ins = i;
+        this.dst_reg_id = reg;
+        this.addr = addr;
     }
 }
 class Register{
@@ -40,58 +141,4 @@ class ReservedStation{
         reg_waited = new Vector<Integer>();
         res_sta_waited = new Vector<Integer>();
     }
-}
-public class Tomasulo {
-
-    Queue<Instruction> ins_queue;
-    Register[] registers = new Register[16];
-    ReservedStation[] stations = new ReservedStation[3+2+3+3];
-        // add*3 multi*2 load*3 store*3
-
-    byte[] mem = new byte[4096];
-    int clock = 0;
-    int add_alu_exec=-1, multi_alu_exec=-1; // id of reserved station which holds the ins executed now
-
-    public static void main(String[] agrs)
-    throws Exception
-    {
-        Tomasulo tomasulo = new Tomasulo();
-    }
-    public Tomasulo()
-    throws Exception
-    {
-        // main func starts here
-        init();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while(br.readLine().length()==0){
-            clock ++;
-            info(clock);
-            // fetch ins
-
-            // update alu infomation. if finished, inform those waiting
-
-            // update UI
-        }
-    }
-
-
-    private void info(String x){
-        // encapsulate print function
-        System.out.println(x);
-    }
-    private void info(int x){
-        // encapsulate print function
-        System.out.println(x);
-    }
-    private void init(){
-        ins_queue = new LinkedList<>();
-        // dequeue:poll()  get head ele:peek()
-        for(int i=0; i<registers.length; i++){
-            registers[i] = new Register();
-        }
-        for(int i=0; i<stations.length; i++){
-            stations[i] = new ReservedStation();
-        }
-    }
-
 }
